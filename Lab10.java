d31f5414904338f3dddf72f860fe4d93674cabb9
ScottMcQuillan <153603539+ScottMcQuillan@users.noreply.github.com>
2023-12-12 10:27:41 +0000

    First Revision Q2

---
+++ Lab10.java
@@ -0,0 +1,210 @@
+import java.util.Random;
+import java.util.Scanner;
+
+public class Lab10 {
+    static Scanner scan = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        Q1();
+        Q2();
+        Q3();
+        Q4();
+        scan.close();
+    }
+
+    public static void Q1() {
+        while (true) {
+            System.out.println("Pick a shape: square, rectangle, circle (or 'q' to quit)");
+            String input = scan.nextLine();
+            if (input.equals("q")) {
+                return;
+            }
+
+            if (input.equals("square")) {
+                double a;
+                System.out.println("Enter the length of side a: ");
+                a = Double.parseDouble(scan.nextLine());
+                System.out.println("The circumference of the square is: " + a * 4);
+                System.out.println("The area of the square is: " + a * a);
+
+            } else if (input.equals("rectangle")) {
+                double a, b;
+                System.out.println("Enter the length of side a: ");
+                a = Double.parseDouble(scan.nextLine());
+                System.out.println("Enter the length of side b: ");
+                b = Double.parseDouble(scan.nextLine());
+                System.out.println("The circumference of the rectangle is: " + (2 * a + 2 * b));
+                System.out.println("The area of the rectangle is: " + (a * b));
+            
+            } else if (input.equals("circle")) {
+                double r;
+                System.out.println("Enter the radius: ");
+                r = Double.parseDouble(scan.nextLine());
+                System.out.println("The circumference of the circle is: " + (Math.PI * r * 2));
+                System.out.println("The area of the circle is: " + (Math.PI * r * r));
+            }
+        }
+    }
+
+    public static void Q2() {
+        System.out.println("Q2: Enter the current day (1-31): ");
+        int day = Integer.parseInt(scan.nextLine()); //gave variable more descriptive name
+        System.out.println("Enter the current month: (1-12)");
+        int month = Integer.parseInt(scan.nextLine()); //gave variable more descriptive name
+
+        if (day >=1 && day <=31){
+            int dayCheck = day%10; //added variable that finds remainder of day/10
+            if(dayCheck == 1) //added if statements that check for each ordinal suffix and prints accordingly
+            {
+              System.out.print("You selected "+day+"st of ");  
+            }
+            else if(dayCheck == 2)
+            {
+              System.out.print("You selected "+day+"nd of ");  
+            }
+            else if(dayCheck == 3)
+            {
+              System.out.print("You selected "+day+"rd of ");  
+            }
+            else if(dayCheck > 3 || dayCheck == 0)
+            {
+              System.out.print("You selected "+day+"th of ");  
+            }
+        }
+        else {
+            System.out.println("Invalid day");
+        }
+
+        //String[] monthNames = new String[12];
+       
+        if(month == 1) {
+            System.out.println("January");
+        } else if (month == 2) {
+            System.out.println("February");
+        } else if (month == 3) {
+            System.out.println("March");
+        } else if (month == 4) {
+            System.out.println("April");
+        } else if (month == 5) {
+            System.out.println("May");
+        } else if (month == 6) {
+            System.out.println("June");
+        } else if (month == 7) {
+            System.out.println("July");
+        } else if (month == 8) {
+            System.out.println("Augst");
+        } else if (month == 9) {
+            System.out.println("September");
+        } else if (month == 10) {
+            System.out.println("October");
+        } else if (month == 11) {
+            System.out.println("November");
+        } else if (month == 12) {
+            System.out.println("December");
+        } else {
+            System.out.println("Invalid month");
+        }
+    }
+
+public static void Q3() {
+    System.out.println("Q3: Enter how many numbers you want to check for primality: ");
+    int n = Integer.parseInt(scan.nextLine());
+    int counter = 0;
+    for (int i = 0; i < n; i++) {
+    if (i < 2)
+    continue;
+    boolean check = true;
+
+    for (int j = 2; j * j <= i; j++) {
+    if (i % j == 0) {
+    check = false;
+    break;
+    } else {
+
+    }
+    }
+        if (check == true) {
+        counter++;
+        } 
+        else {}
+    }
+
+        System.out.println("There are: " + counter + " primes between 0 and " + n);
+    }
+
+    public static void Q4() {
+        Random rng = new Random();
+
+        String next;
+        System.out.println("Q4: Let's play a game. Type \"A\" to attack, \"B\" to buff your next attack. Kill the enemy to win!");
+        System.out.println("Q4: You must roll higher than the enemy armor class (12) to hit. Roll 20 for a critical hit!");
+        System.out.println("Q4: Your damage is 2-16 (2d8)");
+
+        int enemyHP = 100;
+        int a = 0;
+
+        boolean check = false;
+        while (true) {
+
+            boolean doAttack = false;
+            boolean check2 = false;
+            while (!check2) {
+                next = scan.nextLine();
+                check2 = true;
+                switch (next) {
+                    case "A", "a":
+                        doAttack = true;
+                        break;
+                    case "B", "b":
+                        check = true;
+                        System.out.println("Buffing! +5 to your next attack roll and damage");
+                        break;
+                    default:
+                        System.out.println("Invalid input");
+                        check2 = false;
+                }
+            }
+
+            if (doAttack) {
+                a++;
+                int attackRoll = rng.nextInt(20) + 1;
+                int damage = 0;
+                System.out.print("You rolled: " + attackRoll);
+                if(check) {
+                    attackRoll += 5;
+                    System.out.print(" + 5 (buff active)\n");
+                } else {
+                    System.out.println();
+                }
+                if (attackRoll >= 12) {
+                    damage = rng.nextInt(8) + 1;
+                    damage += rng.nextInt(8) + 1;
+                    if(check) {
+                        damage += 5;
+                    }
+                    if (attackRoll == 20 || (check && attackRoll == 20 + 5)) {
+                        damage *= 2;
+                        System.out.print("Critical hit! ");
+                    }
+                    System.out.print("You dealt " + damage + " damage");
+                    if(check) {
+                        System.out.print(" (buffed attack)");
+                    }
+                    enemyHP -= damage;
+                    System.out.println("\nEnemy HP: " + Math.max(0, enemyHP));
+
+                } else {
+                    System.out.println("Miss");
+                }
+
+                check = false;
+                if (enemyHP <= 0) {
+                    System.out.println("Enemy died in " + a + " turns");
+                    scan.close();
+                    return;
+                }
+            }
+
+        }
+    }
+}
